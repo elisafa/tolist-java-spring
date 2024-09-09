d6391e43fac8a26ac43b77223690bea1afc17ffc
Elisafa <elisafajunior@hotmail.com>
2024-09-09 19:09:10 -0300

    criado os arquivos da entidade e repository

--- src/main/java/br/com/elisafa/desafio_todolist/entity/Todo.java
+++ src/main/java/br/com/elisafa/desafio_todolist/entity/Todo.java
@@ -1,2 +1,22 @@
-package br.com.elisafa.desafio_todolist.entity;public class Todo {
+package br.com.elisafa.desafio_todolist.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private boolean realizado;
+    private int prioridade;
 }
--- src/main/java/br/com/elisafa/desafio_todolist/repository/TodoRepository.java
+++ src/main/java/br/com/elisafa/desafio_todolist/repository/TodoRepository.java
@@ -1,2 +1,8 @@
-package br.com.elisafa.desafio_todolist.repository;public class TodoRepository {
+package br.com.elisafa.desafio_todolist.repository;
+
+
+import br.com.elisafa.desafio_todolist.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
 }
